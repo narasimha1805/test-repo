b2af7b3596ce6e6afbddf7261423ad88e3dc4524
narsimulu <narsimulu.b@enhancesys.com>
2016-11-25 20:53:50 +0100

    module 1

--- js/app.js
+++ js/app.js
@@ -1,2 +1,31 @@
-<!-- Declaring angular module-->
-var app = angular.module('notesApp',[]);
+/*(function(){
+  'use strict'
+  var app = angular.module('LunchCheck',[]);
+
+  app.controller('LunchCheckController',LunchCheckController);
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope) {
+    $scope.message = "Hello";
+    $scope.items = "";
+    $scope.lunchMessage = "";
+    $scope.checkTooMuch = function () {
+      if ($scope.items === "") {
+        $scope.lunchMessage = "Please Enter Data First!";
+      }else {
+        var arrayOfStrings = splitString($scope.items,',');
+        if(arrayOfStrings.length > 3) {
+          $scope.lunchMessage = "Too Much!";
+        }else {
+          $scope.lunchMessage = "Enjoy!";
+        }
+      }
+      return $scope.lunchMessage;
+    };
+  }
+    var splitString = function (string, separator) {
+      var arrayOfStrings = string.split(separator);
+      return arrayOfStrings;
+    }
+})();
+*/
+!function(){"use strict";function b(a){a.message="Hello",a.items="",a.lunchMessage="",a.checkTooMuch=function(){if(""===a.items)a.lunchMessage="Please Enter Data First!";else{var b=c(a.items,",");b.length>3?a.lunchMessage="Too Much!":a.lunchMessage="Enjoy!"}return a.lunchMessage}}var a=angular.module("LunchCheck",[]);a.controller("LunchCheckController",b),b.$inject=["$scope"];var c=function(a,b){var c=a.split(b);return c}}();
