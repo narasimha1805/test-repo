6689b273f87f9717053fe6986bf702b620724ecb
narsimulu <narsimulu.b@enhancesys.com>
2016-11-25 21:34:27 +0100

    style changes

--- js/app.js
+++ js/app.js
@@ -1,22 +1,25 @@
-/*(function(){
+(function(){
   'use strict'
   var app = angular.module('LunchCheck',[]);
 
   app.controller('LunchCheckController',LunchCheckController);
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
-    $scope.message = "Hello";
     $scope.items = "";
     $scope.lunchMessage = "";
+    $scope.messageColor = "black";
     $scope.checkTooMuch = function () {
       if ($scope.items === "") {
         $scope.lunchMessage = "Please Enter Data First!";
+        $scope.messageColor = "red";
       }else {
         var arrayOfStrings = splitString($scope.items,',');
         if(arrayOfStrings.length > 3) {
           $scope.lunchMessage = "Too Much!";
+          $scope.messageColor = "green";
         }else {
           $scope.lunchMessage = "Enjoy!";
+          $scope.messageColor = "green";
         }
       }
       return $scope.lunchMessage;
@@ -27,5 +30,5 @@
       return arrayOfStrings;
     }
 })();
-*/
-!function(){"use strict";function b(a){a.message="Hello",a.items="",a.lunchMessage="",a.checkTooMuch=function(){if(""===a.items)a.lunchMessage="Please Enter Data First!";else{var b=c(a.items,",");b.length>3?a.lunchMessage="Too Much!":a.lunchMessage="Enjoy!"}return a.lunchMessage}}var a=angular.module("LunchCheck",[]);a.controller("LunchCheckController",b),b.$inject=["$scope"];var c=function(a,b){var c=a.split(b);return c}}();
+
+/*!function(){"use strict";function b(a){a.message="Hello",a.items="",a.lunchMessage="",a.checkTooMuch=function(){if(""===a.items)a.lunchMessage="Please Enter Data First!";else{var b=c(a.items,",");b.length>3?a.lunchMessage="Too Much!":a.lunchMessage="Enjoy!"}return a.lunchMessage}}var a=angular.module("LunchCheck",[]);a.controller("LunchCheckController",b),b.$inject=["$scope"];var c=function(a,b){var c=a.split(b);return c}}();*/
